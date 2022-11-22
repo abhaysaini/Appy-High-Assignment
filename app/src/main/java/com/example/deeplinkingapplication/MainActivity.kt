e92430863c31299b2c3f8fb52ff90c769a1a97da
abhaysaini <abhaysaini1974@gmail.com>
2022-11-22 14:06:26 +0530

    Initialc

--- app/src/main/java/com/example/deeplinkingapplication/MainActivity.kt
+++ app/src/main/java/com/example/deeplinkingapplication/MainActivity.kt
@@ -13,6 +13,7 @@ import com.google.firebase.dynamiclinks.ktx.androidParameters
 import com.google.firebase.dynamiclinks.ktx.dynamicLink
 import com.google.firebase.dynamiclinks.ktx.dynamicLinks
 import com.google.firebase.ktx.Firebase
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,56 +25,62 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
 
         // to generate deep link
+        generateDeepLink()
 
+        // to get response from deep link
+        recieveDeepLink()
+    }
+
+    private fun recieveDeepLink() {
+        Firebase.dynamicLinks
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData: PendingDynamicLinkData? ->
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    val pm: PackageManager = applicationContext.packageManager
+                    if (!isPackageInstalled("jp.hazuki.yuzubrowser", pm)) {
+                        val intent = Intent(Intent.ACTION_VIEW).apply {
+
+                            // code to redirect user to Google Play Store
+
+                            data =
+                                Uri.parse("https://play.google.com/store/apps/details?id=jp.hazuki.yuzubrowser")
+                            setPackage("com.android.vending")
+                        }
+                        startActivity(intent)
+                    } else {
+
+                        // open the link in app
+
+                        deepLink = pendingDynamicLinkData.link
+                        val url = deepLink.toString()?.substring(20)
+                        binding.webView.loadUrl(url.toString())
+                        binding.webView.settings.javaScriptEnabled = true
+                        Log.i("TAG", url.toString())
+                    }
+
+                }
+            }
+            .addOnFailureListener(this) { e -> Log.w("TAG", "getDynamicLink:onFailure", e) }
+    }
+
+    private fun generateDeepLink() {
         val dynamicLink = Firebase.dynamicLinks.dynamicLink {
 
-//            tab.url
+    //            tab.url
 
-            try{
+            try {
                 link = Uri.parse("https://example.com/https://www.appyhigh.com")
-                domainUriPrefix = "https://mydeeplinkingproject.page.link"
+                domainUriPrefix = "https://mydeeplinkingprojectss.page.link"
                 androidParameters() {}
-            }catch (e : java.lang.Exception){
+            } catch (e: Exception) {
                 e.printStackTrace()
             }
 
         }
 
         val dynamicLinkUri = dynamicLink.uri // final deep link URI
-        Log.i("TAG" , dynamicLinkUri.toString())
-
-
-        // to get response from deep link
-
-        Firebase.dynamicLinks
-            .getDynamicLink(intent)
-            .addOnSuccessListener(this) { pendingDynamicLinkData: PendingDynamicLinkData? ->
-                var deepLink: Uri? = null
-                if (pendingDynamicLinkData != null) {
-                    val pm : PackageManager = applicationContext.packageManager
-                    if(!isPackageInstalled("jp.hazuki.yuzubrowser" , pm)){
-                        val intent = Intent(Intent.ACTION_VIEW).apply {
-
-                            // code to redirect user to Google Play Store
-
-                            data = Uri.parse("https://play.google.com/store/apps/details?id=jp.hazuki.yuzubrowser")
-                                   setPackage("com.android.vending")
-                        }
-                        startActivity(intent)
-                    }else{
-
-                        // open the link in app
-
-                        deepLink = pendingDynamicLinkData.link
-                        val url = deepLink.toString()?.substring(19)
-                        binding.webView.loadUrl(url.toString())
-                        binding.webView.settings.javaScriptEnabled = true
-                        Log.i("TAG" , url.toString())
-                    }
-
-                }
-            }
-            .addOnFailureListener(this) { e -> Log.w("TAG", "getDynamicLink:onFailure", e) }
+        Log.i("TAG", dynamicLinkUri.toString())
     }
 
     private fun isPackageInstalled(packageName: String, packageManager: PackageManager): Boolean {
